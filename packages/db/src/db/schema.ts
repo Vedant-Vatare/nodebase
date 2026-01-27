3620a05ddb064b0c36077e3365a3a43ce52e7bd6
Vedant Vatare <vedantvatare73@gmail.com>
2026-01-27 21:33:27 +0530

    cofigure db package

--- packages/db/src/db/schema.ts
+++ packages/db/src/db/schema.ts
@@ -1,4 +1,14 @@
-import { pgTable, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import {
+	json,
+	pgEnum,
+	pgTable,
+	text,
+	timestamp,
+	uuid,
+	varchar,
+} from "drizzle-orm/pg-core";
+
+export const nodeTypeEnum = pgEnum("nodesEnum", ["action", "trigger"]);
 
 export const usersTable = pgTable("users", {
 	id: uuid().defaultRandom().primaryKey(),
@@ -7,4 +17,14 @@ export const usersTable = pgTable("users", {
 	password: text(),
 	googleOauthId: varchar({ length: 255 }).unique(),
 	createdAt: timestamp().defaultNow(),
+});
+
+export const nodesTable = pgTable("nodes", {
+	id: uuid().defaultRandom().primaryKey(),
+	name: varchar({ length: 255 }).unique().notNull(),
+	task: varchar({ length: 255 }).unique().notNull(),
+	image: text().notNull(),
+	inputs: json(),
+	optionalInputs: json("optional_inputs"),
+	config: json(),
 });
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,37 +1,11 @@
-import "dotenv/config";
-import { eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
-import { usersTable } from "./db/schema";
+import type { Pool } from "pg";
 
-const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
-});
-
-export const db = drizzle({ client: pool, casing: "snake_case" });
-
-async function main() {
-	const user: typeof usersTable.$inferInsert = {
-		name: "John",
-		email: "john@example.com",
-	};
-
-	await db.insert(usersTable).values(user);
-	console.log("New user created!");
-
-	const users = await db.select().from(usersTable);
-	console.log("Getting all users from the database: ", users);
-
-	await db
-		.update(usersTable)
-		.set({
-			name: "edited name",
-		})
-		.where(eq(usersTable.email, user.email));
-	console.log("User info updated!");
-
-	await db.delete(usersTable).where(eq(usersTable.email, user.email));
-	console.log("User deleted!");
+export function createDb(pool: Pool) {
+	return drizzle({
+		client: pool,
+		casing: "snake_case",
+	});
 }
 
-main();
+export * from "./db/schema";
