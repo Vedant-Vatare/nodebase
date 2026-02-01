8691c0119a924fd2273118a2b9add77cd3395296
Vedant Vatare <vedantvatare73@gmail.com>
2026-02-01 18:27:24 +0530

    API (server): add JWT for user auth

--- apps/server/src/controllers/auth.controller.ts
+++ apps/server/src/controllers/auth.controller.ts
@@ -3,7 +3,7 @@ import type { userSignup } from "@nodebase/shared";
 import type { Request, Response } from "express";
 import createHttpError from "http-errors";
 import { isDBQueryError } from "@/utils/api.utils.js";
-import { bcryptHash } from "@/utils/auth.utils.js";
+import { bcryptHash, createJWT } from "@/utils/auth.utils.js";
 
 export const Signup = async (req: Request, res: Response) => {
 	try {
@@ -13,13 +13,20 @@ export const Signup = async (req: Request, res: Response) => {
 		const [user] = await db
 			.insert(usersTable)
 			.values({ name, email, password: hashedPassword })
-			.returning();
+			.returning({
+				id: usersTable.id,
+				name: usersTable.name,
+				email: usersTable.email,
+				created_at: usersTable.createdAt,
+			});
 
 		if (!user) {
 			throw createHttpError(400, "Failed to create user");
 		}
-
-		return res.status(201).json({ message: "user signup success", user });
+		const token = await createJWT({ userId: user.id });
+		return res
+			.status(201)
+			.json({ message: "user signup success", user, token });
 	} catch (e) {
 		const queryError = isDBQueryError(e);
 
--- apps/server/src/utils/auth.utils.ts
+++ apps/server/src/utils/auth.utils.ts
@@ -1,5 +1,19 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const bcryptHash = async (data: string) => {
 	return bcrypt.hash(data, 12);
+};
+
+export const createJWT = async (
+	data: string | Record<string, string> | Buffer,
+) => {
+	return jwt.sign(data, process.env.JWT_secret as string, {
+		algorithm: "HS256",
+		expiresIn: "14d",
+	});
+};
+
+export const verifyJWT = async (token: string) => {
+	return jwt.verify(token, process.env.JWT_secret as string);
 };
