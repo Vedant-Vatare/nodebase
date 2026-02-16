b70e17e664bbe754464534f8ad628793c0ef89aa
Vedant Vatare <vedantvatare73@gmail.com>
2026-02-16 22:39:26 +0530

    add tanstack router for routing

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,15 +1,29 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import { routeTree } from './routeTree.gen'
+import "./index.css"
 
-const root = document.getElementById("root");
+const router = createRouter({ routeTree })
 
-if (!root) {
-	throw new Error("Root element not found");
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
 }
-createRoot(root).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-);
+
+const rootElement = document.getElementById('root')
+if (!rootElement) throw new Error("root element is not defined")
+
+
+
+
+
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  )
+}
---
+++ apps/client/src/routes/auth/signup.tsx
@@ -0,0 +1,6 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { Signup } from "@/pages/signup";
+
+export const Route = createFileRoute("/auth/signup")({
+	component: Signup,
+});
